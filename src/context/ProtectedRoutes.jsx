9bdc5ff3c9b64b1d7c4b24df99e5d47d6c8511d3
Mehroz farooq <85057779+Mehroz101@users.noreply.github.com>
2025-01-16 12:13:35 +0500

    change code in protected route

--- src/context/ProtectedRoutes.jsx
+++ src/context/ProtectedRoutes.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 const ProtectedRoute = ({ element }) => {
   const navigate = useNavigate();
   const [isAuthenticated, setIsAuthenticated] = useState(true);
